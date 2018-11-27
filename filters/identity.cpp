f9ef8a0122daee9b14220f3369c95459159dded6
Bruno Bodin <bbodin@inf.ed.ac.uk>
2018-11-27 14:25:17 +0800

    Identity proxy Frame

--- filters/identity.cpp
+++ filters/identity.cpp
@@ -6,9 +6,32 @@
  */
 
 #include <SLAMBenchAPI.h>
+#include <io/SLAMFrame.h>
+
+
+class SLAMIdentityFrame : public slambench::io::SLAMFrame {
+private:
+	slambench::io::SLAMFrame * _from;
+public :
+
+	// TODO: I have some issues with this, we should have a way to copy SLAMFrame in a safer manner.
+	SLAMIdentityFrame(slambench::io::SLAMFrame * from) : _from (from) {
+		this->FrameSensor = from->FrameSensor;
+		this->Timestamp = from->Timestamp;
+		if (this->FrameSensor->IsVariableSize()) {
+			this->SetVariableSize(from->GetVariableSize());
+		}
+
+	}
+	void *GetData() override { return _from->GetData();  };
+	void FreeData() override { return _from->FreeData(); };
+
+};
+
+
 
  slambench::io::SLAMFrame * sb_filter (SLAMBenchFilterLibraryHelper * , slambench::io::SLAMFrame * frame) {
-	return frame;
+	return new SLAMIdentityFrame(frame);
 }
 
 
