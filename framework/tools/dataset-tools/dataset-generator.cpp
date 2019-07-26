e751fd20758974ccca8864b563c0f5a9e5cabc79
Matthew Spear <matthewtimothyspear@gmail.com>
2019-07-26 23:24:54 +0100

    Refactor dataset-generator
    - clear unused imports
    - adjust formatting
    - modernise code using auto
    - tidy up command-line outputs

--- framework/tools/dataset-tools/dataset-generator.cpp
+++ framework/tools/dataset-tools/dataset-generator.cpp
@@ -14,22 +14,11 @@
 #include <io/SLAMFile.h>
 #include <io/SLAMFrame.h>
 #include <io/format/PointCloud.h>
-#include <io/sensor/CameraSensor.h>
-#include <io/sensor/DepthSensor.h>
-#include <io/sensor/GroundTruthSensor.h>
-#include <io/sensor/PointCloudSensor.h>
-#include <io/sensor/Sensor.h>
-#include <Eigen/Eigen>
-
-#include <fstream>
-#include <iomanip>
-#include <map>
-#include <sstream>
-#include <string>
-
-#include <cmath>
 
 #include <unistd.h>
+#include <cmath>
+#include <sstream>
+#include <string>
 
 #include "./include/BONN.h"
 #include "./include/EUROCMAV.h"
@@ -43,7 +32,7 @@ using namespace slambench::io;
 class MainComponent : public ParameterComponent {
  private:
   slambench::ParameterManager param_manager;
-  DatasetReader* reader = nullptr;
+  DatasetReader *reader = nullptr;
 
  public:
   std::string binary_name;
@@ -52,32 +41,33 @@ class MainComponent : public ParameterComponent {
   bool quiet;
 
  public:
-  static void help_callback(Parameter*, ParameterComponent* caller) {
-    MainComponent* config = dynamic_cast<MainComponent*>(caller);
+  static void help_callback(Parameter *, ParameterComponent *caller) {
 
-    std::cerr << "" << std::endl;
-    std::cerr << " == =========================== ==" << std::endl;
-    std::cerr << " == SLAMBench Dataset Generator ==" << std::endl;
-    std::cerr << " == =========================== ==" << std::endl;
-    std::cerr << "" << std::endl;
-    std::cerr << "Description: "
-              << "The dataset generator is used to produce *.slam files "
-                 "(dataset files compatible with the SLAMBench loader)."
-              << "" << std::endl;
+    auto config = dynamic_cast<MainComponent *>(caller);
 
-    std::cerr << "Usage: " << config->binary_name
-              << " -d dataset-type -o output-slam-file [ with extra arguments "
-                 "required for the different dataset type]"
-              << std::endl;
-    std::cerr << "  Available parameters :" << std::endl;
+    std::cerr << "\n"
+                 "== =========================== ==\n"
+                 "== SLAMBench Dataset Generator ==\n"
+                 "== =========================== ==\n"
+                 "\n"
+                 "Description:\n   The dataset generator is used to produce *.slam files"
+                 "(dataset files compatible with the SLAMBench loader).\n"
+                 "\n"
+                 "Usage:\n   " << config->binary_name << " -d dataset-type -o output-slam-file "
+                  "[with extra arguments required for the different dataset type]\n"
+                  "\n"
+                  "Parameters:\n";
+
     config->param_manager.PrintArguments(std::cerr);
 
     exit(0);
   }
 
-  static void dataset_callback(Parameter* p, ParameterComponent* caller) {
-    MainComponent* config = dynamic_cast<MainComponent*>(caller);
-    TypedParameter<std::string>* dataset = dynamic_cast<TypedParameter<std::string>*>(p);
+  static void dataset_callback(Parameter *p, ParameterComponent *caller) {
+
+    auto config = dynamic_cast<MainComponent *>(caller);
+
+    auto dataset = dynamic_cast<TypedParameter<std::string> *>(p);
     std::string dataset_name = dataset->getTypedValue();
 
     if (dataset_name == "iclnuim") {
@@ -119,69 +109,70 @@ class MainComponent : public ParameterComponent {
     }
     printf("] ");
 
-    printf("%u / %u", idx, total);
+    printf("%i / %i", idx, total);
     fflush(stdout);
   }
-  MainComponent(int argc, char* argv[]) : ParameterComponent(""), binary_name(argv[0]) {
 
-      this->addParameter(TypedParameter<std::string>("d", "dataset",
-                                                     "Name of the input dataset type "
-                                                     "(iclnuim, tum, eurocmav, icl, svo, bonn)",
-                                                     &this->dataset, NULL, this->dataset_callback));
+  MainComponent(int argc, char *argv[]) : ParameterComponent(""), binary_name(argv[0]), quiet(false) {
 
-      this->addParameter(TypedParameter<std::string>("o", "log-file",
-                                                     "Output slam file",
-                                                     &this->output, NULL));
+    this->addParameter(TypedParameter<std::string>("d", "dataset",
+                                                   "Name of the input dataset type "
+                                                   "(iclnuim, tum, eurocmav, icl, svo, bonn)",
+                                                   &this->dataset, nullptr, this->dataset_callback));
 
-      this->addParameter(TypedParameter<bool>("q", "quiet",
-                                              "Hide the progress bar",
-                                              &this->quiet, NULL));
+    this->addParameter(TypedParameter<std::string>("o",
+                                                   "log-file",
+                                                   "Output slam file", &this->output, nullptr));
 
-      this->addParameter(TriggeredParameter("h", "help",
-                                            "Print the help.",
-                                            this->help_callback));
+    this->addParameter(TypedParameter<bool>("q", "quiet",
+                                            "Hide the progress bar", &this->quiet, nullptr));
+
+    this->addParameter(TriggeredParameter("h", "help",
+                                          "Print the help.", this->help_callback));
 
     this->param_manager.AddComponent(this);
     this->param_manager.ReadArguments(argc, argv, this);
   }
 
   bool Run() {
-    SLAMFile* slamfile = reader->GenerateSLAMFile();
-    if (slamfile == nullptr) {
+
+    SLAMFile *slam_file = reader->GenerateSLAMFile();
+
+    if (slam_file == nullptr) {
       std::cout << "No SLAM File generated." << std::endl;
       return false;
     }
-    std::cout << "Writing output." << std::endl;
 
-    return SLAMFile::Write(this->output, *slamfile,
-                           this->quiet ? nullptr : frame_callback);
+    std::cout << "Writing output." << std::endl;
+    return SLAMFile::Write(this->output, *slam_file, this->quiet ? nullptr : frame_callback);
   }
 };
 
-int main(int argc, char* argv[]) {
-  MainComponent* main = new MainComponent(argc, argv);
+int main(int argc, char *argv[]) {
 
-  if (main->dataset == "") {
-      std::cout << " Please define the dataset type using the -d argument.\n"
-                   " Possible values are: iclnuim tum eurocmav icl svo bonn\n"
-                   " To have details of arguments for any type of dataset you are interested by,\n"
-                   " Please run the help mode for this dataset (e.g " << argv[0] << "-d tum)\n";
+   auto main = new MainComponent(argc, argv);
+
+  if (main->dataset.empty()) {
+    std::cout << "   Please define the dataset type using the -d argument.\n"
+                 "   Possible values are: iclnuim tum eurocmav icl svo bonn\n\n"
+                 "   To have details of arguments for any type of dataset you are interested by,\n"
+                 "   Please run the help mode for this dataset (e.g " << argv[0] << " -d tum)\n";
     return EXIT_FAILURE;
   }
 
-  if (main->output == "") {
-    std::cout << " Please define the output file using the -o argument. " << std::endl;
+  if (main->output.empty()) {
+    std::cout << "Please define the output file using the -o argument.\n";
     return EXIT_FAILURE;
   }
 
   std::cout << "Selected Dataset is " << main->dataset << std::endl;
   std::cout << "Selected output is " << main->output << std::endl;
 
-  std::cout << "Run generation..." << std::endl;
+  std::cout << "Run generation...\n";
 
   bool res = main->Run();
 
-  std::cout << std::endl << "Done." << std::endl;
+  std::cout << std::endl << "Done.\n";
 
   assert(res);
 }
