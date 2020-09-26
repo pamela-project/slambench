e106c2ec9a7adee2054960e6f7fe4015c33d92c6
Bruno <bruno.bodin@yale-nus.edu.sg>
2020-09-26 11:12:40 +0800

    Fixed for MACOS

--- framework/shared/include/timings.h
+++ framework/shared/include/timings.h
@@ -11,42 +11,13 @@
 #ifndef TIMINGS_H
 #define TIMINGS_H
 
-#ifdef __APPLE__
-#include <sys/time.h>       /* clock_t, clock, CLOCKS_PER_SEC */
-#else
 #include <chrono>
-#endif
-
-/*
-inline float tick() {
-
-    static struct timeval t;
-
-    struct timeval diff = t;
-    gettimeofday(&t, NULL);
-
-    return ((t.tv_sec - diff.tv_sec) * 1000000u + t.tv_usec - diff.tv_usec)
-            / 1.e6;
-
-}
-*/
-
-
 
 inline double tock() {
-#ifdef __APPLE__
-		clock_serv_t cclock;
-		mach_timespec_t clockData;
-		host_get_clock_service(mach_host_self(), SYSTEM_CLOCK, &cclock);
-		clock_get_time(cclock, &clockData);
-		mach_port_deallocate(mach_task_self(), cclock);
-		return (double) clockData.tv_sec + clockData.tv_nsec / 1000000000.0;
-#else
 		static auto base = std::chrono::high_resolution_clock::now();
 		auto end_of_computation = std::chrono::high_resolution_clock::now();
 		std::chrono::duration<double> total_time =  end_of_computation - base;
 		return total_time.count();
-#endif
 }
 
 #endif //TIMINGS_H
